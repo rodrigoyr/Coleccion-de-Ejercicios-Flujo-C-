6b34aaf9c9a02e5935a873ae226728ae603895fe
rodrigoyr <ryeperub@myuax.com>
2023-11-23 12:53:45 +0100

    new 2 code commit

--- Ejercicio 1/main.cpp
+++ Ejercicio 1/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 
 int main() {
     int num;
+    std::string text;
     std::cout << "Ingresa un numero: ";
     std::cin >> num;
-    std::cout << "El numero ingresado es: " << num << std::endl;
+    std::cout << "Ingresa un texto: ";
+    std::cin >> text;
+    std::cout << "El numero ingresado es: " << num << "\nEl texto ingresado es: " << text << std::endl;
     return 0;
 }
