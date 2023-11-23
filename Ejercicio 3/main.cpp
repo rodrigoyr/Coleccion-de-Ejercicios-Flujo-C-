1b17394a0601da5ac02a367b212a5d6869a1fd59
rodrigoyr <ryeperub@myuax.com>
2023-11-23 12:58:57 +0100

    code commit

--- Ejercicio 3/main.cpp
+++ Ejercicio 3/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string text;
+    std::cout << "Ingresa un texto: ";
+    std::getline(std::cin, text);
+
+    std::ofstream file;
+    file.open("example.txt");
+    if (file) {
+        file << text;
+        std::cout << "El texto se guardó correctamente.\n";
+    } else {
+        std::cerr << "No se pudo abrir el archivo.\n";
+    }
+    file.close();
     return 0;
 }
