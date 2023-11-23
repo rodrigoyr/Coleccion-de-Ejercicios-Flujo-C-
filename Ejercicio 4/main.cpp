02bc6a8a3c9b3d4cf890fc19c5e5dff98146d0f1
rodrigoyr <ryeperub@myuax.com>
2023-11-23 13:00:12 +0100

    final code commit

--- Ejercicio 4/main.cpp
+++ Ejercicio 4/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <sstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string text;
+    std::cout << "Ingresa un texto: ";
+    std::getline(std::cin, text);
+
+    std::stringstream ss;
+    ss << text;
+    std::string text_from_ss;
+    ss >> text_from_ss;
+    std::cout << "El texto desde el flujo de cadena es: " << text_from_ss << std::endl;
     return 0;
 }
