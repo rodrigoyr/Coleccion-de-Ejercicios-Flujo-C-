b48a0436bfa616981e3ed66b20c78376aba4ef2b
rodrigoyr <ryeperub@myuax.com>
2023-11-23 12:54:59 +0100

    code commit

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ifstream file;
+    file.open("example.txt");
+    if (!file) {
+        std::cerr << "No se pudo abrir el archivo.\n";
+    } else {
+        std::cout << "El archivo se abrió correctamente.\n";
+    }
+    file.close();
     return 0;
 }
